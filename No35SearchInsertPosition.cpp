cc6ae71a8f7e7971b4fe89aa2887956b11822439
wyw97 <760115908@qq.com>
2018-09-07 09:06:53 +0800

    Create No35SearchInsertPosition.cpp

---
+++ No35SearchInsertPosition.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int len=nums.size();
+        for(int i=0;i<len;i++){
+            if(nums[i]>=target) return i;
+            //else if(nums[i]>target) return i; 
+        }
+        return len;
+    }
+};
