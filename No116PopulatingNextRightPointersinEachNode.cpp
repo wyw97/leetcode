a7f447949fcd6e03304be5a5a6067eb186f92501
Yiwen Wang <760115908@qq.com>
2019-01-24 20:58:30 +0800

    Create No116PopulatingNextRightPointersinEachNode.cpp

---
+++ No116PopulatingNextRightPointersinEachNode.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if(!root) return;
+        if(!root->left) return;
+        root->left->next = root->right;
+        if(root->next){
+            root->right->next = root->next->left;
+        }
+        connect(root->left);
+        connect(root->right);
+    }
+};
+
+/*
+      完全二叉树的递归搜索;
+      在进入下一层之前，先修改下一层的子节点;
+
+*/
